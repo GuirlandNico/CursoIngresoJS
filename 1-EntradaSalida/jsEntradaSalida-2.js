dfecc350cca21916089b99fa1c06c26c28f11348
GuirlandNico <40776505+GuirlandNico@users.noreply.github.com>
2018-07-02 21:54:31 -0300

    Curso de ingreso
    Primera Clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,18 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	var Nombre;
+//Nombre = "Nico";
+//alert (Nombre);
+	Nombre = prompt("Ingrese su nombre", "Pablito Lescano");
+	alert("Su Nombre es: " + Nombre);
+
+//comentario de una linea
+//var person = prompt("Please enter your name", "Harry Potter"); Esto lo sacamos de w3school
+	
+/*
+
+*/
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,21 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	alert(nombre);
 	
+/*
+pasos 
+1 document.
+2 Tecla g
+3 Tecla TAB
+4 Tecla Shift, Tecla i
+
+sigue
+
+.value es lo que da valor a lo ingresado
+ en elcuadro de texto de la pagina,dependiendo
+  del elemento que se este llamando. En este caso Nombre
+*/
 }
-
-

b5aa4b0d660d5d48e68634326630c13b6eb033f4
GuirlandNico <40776505+GuirlandNico@users.noreply.github.com>
2018-07-26 18:01:16 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var precioF;
+
+
+	precio = prompt ("el precio es: ");
+	precio = parseInt (precio);
+	porcentaje = prompt ("El porcentaje es: " );
+	porcentaje = parseInt (porcentaje);
+
+	descuento=precio*porcentaje/100;
+	precioF=precio-descuento;
+
+	document.getElementById('elPrecioFinal')=precioF;
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho = prompt("Ingrese el ancho: ");
+	ancho = parseInt (ancho);
+	largo = prompt ("ingrese el largo: ");
+	largo = parseInt (largo); 
+	perimetro= (ancho+largo)*2;
+
+	alert ("el perimetro es: "+perimetro);
+
 
 }

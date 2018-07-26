46b1feebc486e85314790a6fbb2ac5fe7fdae64b
GuirlandNico <40776505+GuirlandNico@users.noreply.github.com>
2018-07-26 19:31:12 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-	alert('iteración while');
+		var contador;
 
+	contador=10;
 
+	while (contador>0)
+	{
+		console.log ("numero"+contador);
+		contador--;
+
+		alert('iteración while: '+contador );
+
+	}
 }//FIN DE LA FUNCIÓN

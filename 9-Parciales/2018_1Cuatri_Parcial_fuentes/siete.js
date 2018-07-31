3fcdc31ed151622d4a24734d298657b8395a5480
GuirlandNico <40776505+GuirlandNico@users.noreply.github.com>
2018-07-30 22:12:08 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,64 @@
+
+/* 
+jose m 25
+maria f 16
+jesus m 33
+fer f  81
+*/
 function mostrar()
+{	var contador=0;
+	var nombre;
+	var sexo; var cantMujeres=0; var cantHombres=0;
+	var edad;
+	var mayores=0;
+	var menores=0;
+
+	  while (contador<4)
+	{
+	contador++;
+	nombre=prompt("ingrese nombre: ");
+
+	sexo=prompt("ingrese sexo: ");
+	while (sexo!="m" && sexo!="f")
+	{
+		sexo=prompt("ingrese sexo valido:")
+
+	}
+	edad=prompt("ingrese edad: ")
+	edad=parseInt(edad);
+	while (isNaN(edad)||edad<0||edad>100)
 {
+	edad=prompt("ingrese edad: ")
+	edad=parseInt(edad);
+}
+
+if (sexo=="f")
+		{
+		cantMujeres++;
+		}
+else
+		{ cantHombres++;
+	 	}
+
+if (edad>18)
+		{
+		mayores++;
+		}
+else
+		{ menores++;
+	 	}
+
+
+}
+
+
+document.write("cantMujeres:"+cantMujeres+ "<BR>");
+
+document.write("cantHombres:"+cantHombres+ "<BR>");
+
+document.write("mayores:"+mayores+ "<BR>")
+
+document.write("menores:"+menores+ "<BR>")
+
 
 }
